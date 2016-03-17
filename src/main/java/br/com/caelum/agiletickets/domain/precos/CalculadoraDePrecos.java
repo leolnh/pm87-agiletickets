07b952a96bc6721a36f0d7b6e8574ed48077e098
Leonardo Hernandes <leo.lnh@gmail.com>
2016-03-16 22:12:15 -0300

    tira elses

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -14,30 +14,36 @@ public class CalculadoraDePrecos {
 		case SHOW:
 			//quando estiver acabando os ingressos... 
 			if(estaNosUltimos(sessao, 0.05)) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
+				preco = aumentaPreco(sessao, 0.10);
 			}
 			break;
 		case BALLET:
 			if(estaNosUltimos(sessao, 0.50)) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
+				preco = aumentaPreco(sessao, 0.20);
+				
 			} 
 			
 			if(sessao.getDuracaoEmMinutos() > 60){
-				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
+				preco = aumentaPreco(sessao, 0.10);
 			}
 			break;
 		case ORQUESTRA:
 			if(estaNosUltimos(sessao, 0.50)) {
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
+				preco = aumentaPreco(sessao, 0.20);
 			} 
 
 			if(sessao.getDuracaoEmMinutos() > 60){
+				//preco = aumentaPreco(sessao, 0.10);
 				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
 			}
 			break;
 		}
 		
 		return preco.multiply(BigDecimal.valueOf(quantidade));
+	}
+
+	private static BigDecimal aumentaPreco(Sessao sessao, double porcentagemDeAumento) {
+		return  sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(porcentagemDeAumento)));
 	}
 
 	private static boolean estaNosUltimos(Sessao sessao, Double porcentagemDeMaisCaros) {
